f3ae643e82cb47669492da6114d3638f7bd2d169
sooooooooooo <zhigengqi@yahoo.com>
2020-11-27 21:10:07 -0600

    setup react client side with create-react-app and material-ui

---
+++ client/src/Components/App/App.js
@@ -0,0 +1,42 @@
+import logo from './../../logo.svg';
+import './App.css';
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import { Button, Paper } from "@material-ui/core";
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Container maxWidth="sm" className="App">
+      <Paper>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Typography variant="h4" component="h1" gutterBottom>
+          To-Do List Reminders App
+        </Typography>
+        <Button variant="contained" color="primary">
+          Primary Button
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary Button
+        </Button>
+      </Paper>
+    </Container>
+  );
+}
+
+export default App;
